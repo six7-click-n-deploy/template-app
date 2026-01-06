8084d99d66fe1914da2ceaba19d263f7ae4518f0
Leon Priemer <leon.priemer@gmail.com>
2026-01-06 22:16:52 +0100

    Testing CICD

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,10 +2,6 @@ terraform {
   required_version = ">= 1.0"
 
   required_providers {
-    openstack = {
-      source  = "terraform-provider-openstack/openstack"
-      version = "~> 1.53"
-    }
     random = {
       source  = "hashicorp/random"
       version = "~> 3.5"
